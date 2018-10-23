c24212c131e42781d3346babbeaf77f635eaa0a2
chrismanp <chrismanp92@outlook.com>
2018-10-23 06:22:29 -0500

    Fix error

--- build_linux_firsttime.sh
+++ build_linux_firsttime.sh
@@ -1,18 +1,40 @@
 sudo apt-get update
 
+read -p "Press any key"
+
 sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc
 
+read -p "Press any key"
+
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.1.tar.xz
 tar xf linux-4.7.1.tar.xz
+
+read -p "Press any key"
 
 cd linux-4.7.1
 
+read -p "Press any key"
+
+
 cp /boot/config-$(uname -r) .config   
+
+read -p "Press any key"
+
 
 make menuconfig
 
+read -p "Press any key"
+
+
 sudo make -j 4 && sudo make modules_install -j 4 && sudo make install -j 4
 
-update-initramfs -c -k 4.7.1   
+read -p "Press any key"
 
-update-grub  
+
+sudo update-initramfs -c -k 4.7.1   
+
+read -p "Press any key"
+
+
+sudo update-grub  
 
